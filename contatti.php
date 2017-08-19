ef4c07cdb37e687cbbb5b1f62773cdf12ae40d5a
Matteo Alesiani <teo97.alesiani@gmail.com>
2017-08-19 18:33:59 +0200

    Add file

--- contatti.php
+++ contatti.php
@@ -1,9 +1,16 @@
 <?php
 if(isset($_POST['submit']))
 {
+	$nome = trim(strip_tags($_POST['form1-3-name']));
+	$email = trim(strip_tags($_POST['form1-3-email']));
+	$messaggio = trim(strip_tags($_POST['form1-3-message']));
+	$telefono = trim(strip_tags($_POST['form1-3-phone']));
+	
+	$header = "From: $email\n" . "Reply-To: $email\n" . "Cell: $telefono\n";
 	$subject = "Richiesta dal Sito-Web";
 	$email_to = "teo97.alesiani@gmail.com ";
 
+	if(mail($email_to, $subject, $messaggio, $header)) {
 		echo "Mail inviata con successo";
 	}
 	else {
